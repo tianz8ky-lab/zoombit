638c360dfd15f2664a3d4f1b63ee1745ee6b1a8e
tianz8ky-lab <tianz8ky@gmail.com>
2025-12-10 15:34:40 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+basic.showIcon(IconNames.Square)
+let position = 0
+while (!(input.buttonIsPressed(Button.A))) {
+	
+}
+basic.forever(function () {
+    if (true) {
+    	
+    } else if (false) {
+        position = 1
+    } else if (false) {
+        position = 1
+    } else if (false) {
+        position = 1
+    } else if (false) {
+        position = 1
+    } else if (false) {
+        if (position == 1) {
+        	
+        } else if (position == 2) {
+        	
+        } else {
+        	
+        }
+    } else {
+    	
+    }
+})
